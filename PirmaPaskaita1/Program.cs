ed7517ab49ebbf331a3b399f72eb7b33f4618244
Sarunas <sarugen12@gmail.com>
2024-10-22 08:07:01 +0200

    Add project files.

---
+++ PirmaPaskaita1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PirmaPaskaita1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite sveikaji skaiciu: ");
+            int ivestasSkaicius = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Ivestas skaicius {ivestasSkaicius}");
+
+
+            Console.WriteLine("Iveskite sveikaji skaiciu: ");
+            bool arIvesta = int.TryParse(Console.ReadLine(), out int saugiaiIvestas);
+
+            Console.WriteLine($"Saugiai ivestas: {saugiaiIvestas}");
+        }
+    }
+}
---
+++ PirmaUzduotis/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PirmaUzduotis
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite staciakampio ilgi (cm): ");
+            double ilgis = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Iveskite staciakampio ploti (cm): ");
+            double plotis = double.Parse(Console.ReadLine());
+
+            double plotas = ilgis * plotis;
+
+            Console.WriteLine($"Staciakampio plotas {plotas}cm" );
+        }
+    }
+}
